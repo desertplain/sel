2b5cb89564fd5bcc61e85aeb45500d700dd218b5
desertplain <desertplain@users.noreply.github.com>
2020-02-23 14:25:26 +0100

    niedziela

---
+++ sel5.py
@@ -0,0 +1,104 @@
+#python 2 + selenium
+
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+from time import sleep
+import unittest
+
+email='llolla@op.pl'
+fname='misiek'
+lname='misiowy'
+haslo='haslo'
+birthday='3'
+birthmonth='12'
+birthyear='2000'
+adres='radosna 1'
+miasto='wroclaw'
+stan='Alabama'
+kod='55-555'
+kraj='United States'
+fon='121212121'
+aliasss='buda'
+
+print 'hellou mai frend'
+
+# tworzymy klase wspplcheck dziedziczaca po z modulu unittest
+
+class APRegistration(unittest.TestCase):
+    """"scenariusz testowy"""
+    # Warunki wstepne
+    # Przygotowanie testow
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+        self.driver.get('http://automationpractice.com')
+        self.driver.implicitly_wait(10)
+    #wlasciwe testy
+    def testCorrectRegitration(self):
+        #odnajdz i kliknij sign in ,
+        driver=self.driver
+        driver.find_element_by_class_name('login').click()
+
+        driver.find_element_by_id('email_create').send_keys(email)
+
+        driver.find_element_by_id('SubmitCreate').click()
+
+        driver.find_element_by_id('customer_firstname').send_keys(fname)
+
+        driver.find_element_by_id('id_gender1').click()
+
+        driver.find_element_by_id('customer_lastname').send_keys(lname)
+
+        driver.find_element_by_id('passwd').send_keys(haslo)
+
+        Select(driver.find_element_by_id('days')).select_by_value(birthday)
+
+        Select(driver.find_element_by_id('months')).select_by_value(birthmonth)
+
+        Select(driver.find_element_by_id('years')).select_by_value(birthyear)
+
+        email_text=driver.find_element_by_id('email').get_attribute('value')
+        assert email_text == email
+        print email_text
+
+        fname_text=driver.find_element_by_id('firstname').get_attribute('value')
+        assert fname_text == fname
+        print fname_text
+
+        lname_text=driver.find_element_by_id('lastname').get_attribute('value')
+        assert lname_text == lname
+        print lname_text
+
+        driver.find_element_by_id('address1').send_keys(adres)
+
+        driver.find_element_by_id('city').send_keys(miasto)
+
+        driver.find_element_by_id('id_state').send_keys(stan)
+
+        driver.find_element_by_id('postcode').send_keys(kod)
+
+        driver.find_element_by_id('id_country').send_keys(kraj)
+
+        driver.find_element_by_id('phone_mobile').send_keys(fon)
+
+        driver.find_element_by_id('alias').clear()
+        driver.find_element_by_id('alias').send_keys(aliasss)
+
+        driver.find_element_by_id('submitAccount').click()
+
+        sleep(10)
+        #self.assertIn('Bankowe',driver.title)
+        #print(driver.title)
+
+
+    # sprztanie
+    def tearDown(self):
+        self.driver.quit()
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
+
+
+
+    """"
+
+    """
---
+++ sel6.py
@@ -0,0 +1,142 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+from time import sleep
+import unittest
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+
+email='llolla@op.pl'
+birthday='3'
+aliasss='buda'
+
+print('hellou mai frend')
+
+# tworzymy klase wspplcheck dziedziczaca po z modulu unittest
+
+class WizRegistration(unittest.TestCase):
+    """"scenariusz testowy"""
+    # Warunki wstepne
+    # Przygotowanie testow
+    def setUp(self):
+        profile = webdriver.FirefoxProfile()
+        profile.set_preference("geo.enabled", False)
+        self.driver=webdriver.Firefox(profile)
+        # profile.set_preference("geo.prompt.testing", True)
+        self.driver.maximize_window()
+        self.driver.get('https://wizzair.com/pl-pl/#/')
+
+        #self.driver.implicitly_wait(50)
+    #wlasciwe testy
+    def testWrongRegitration(self):
+        #odnajdz i kliknij sign in ,
+        dr=self.driver
+        imie='misiek'
+        nazwisko='misiowy'
+        wait_onweb=WebDriverWait(dr, 50)
+        gender='female'
+        fon='111111111'
+        emaijl='misiek.op.pl'
+        haslo='Olaboga7'
+        narod='Polska'
+        button_sign1='//button[@data-test="navigation-menu-signin"]'
+        button_sign2='//button[text()=" Rejestracja "]'
+        pole1='firstName'
+        pole2='lastName'
+        kod_kraju='+48'
+        pole3='phone-number-country-code'
+        button_sign3='//label[@data-test="register-gendermale"]'
+        button_sign4='//label[@data-test="register-genderfemale"]'
+        button_sign5='//div[@data-test="booking-register-country-code"]'
+        button_sign6='//input[@name="phone-number-country-code"]'
+        button_sign7='//li[@data-test="PL"]'
+        pole4='phoneNumberValidDigits'
+        pole5='email'
+        pole6='//input[@data-test="booking-register-password"]'
+        pole7='//input[@data-test="booking-register-country"]'
+        pole8="//div[@class='register-form__country-container__locations']"
+        pole9="label"
+        check1='//label[@for="registration-privacy-policy-checkbox"][@class="rf-checkbox__label"]'
+
+        wait_onweb.until(EC.element_to_be_clickable((By.XPATH,button_sign1))).click()
+        wait_onweb.until(EC.element_to_be_clickable((By.XPATH,button_sign2))).click()
+        wpis_lewy=wait_onweb.until(EC.element_to_be_clickable((By.NAME,pole1)))
+        wpis_prawy=wait_onweb.until(EC.element_to_be_clickable((By.NAME,pole2)))
+        #wait_onweb.until(EC.element_to_be_clickable((By.NAME,pole1))).send_keys(imie)
+        #wait_onweb.until(EC.element_to_be_clickable((By.NAME,pole2))).send_keys(nazwisko)
+        if gender=='male':
+            wpis_prawy.send_keys(nazwisko)
+            wpis_lewy.send_keys(imie)
+            dr.find_element_by_xpath(button_sign3).click()
+        else:
+            wpis_lewy.send_keys(imie)
+            wpis_prawy.send_keys(nazwisko)
+            dr.find_element_by_xpath(button_sign4).click()
+
+        wait_onweb.until(EC.element_to_be_clickable((By.XPATH,button_sign5))).click()
+        wait_onweb.until(EC.element_to_be_clickable((By.XPATH,button_sign6))).send_keys(kod_kraju)
+        wait_onweb.until(EC.element_to_be_clickable((By.XPATH,button_sign7))).click()
+
+        dr.find_element_by_name(pole4).send_keys(fon)
+
+        dr.find_element_by_name(pole5).send_keys(emaijl)
+
+        dr.find_element_by_xpath(pole6).send_keys(haslo)
+
+        dr.find_element_by_xpath(pole7).click()
+        # Wyszukaj kraje
+        country_to_choose = dr.find_element_by_xpath(pole8)
+        # Poszukaj elementow "label" wewnatrz listy "countries"
+        countries = country_to_choose.find_elements_by_tag_name(pole9)
+        # Iteruj po kazdym elemencie w liscie "countries"
+        for label in countries:
+            # Wewnatrz "label" znajdz element "strong"
+            option=label.find_element_by_tag_name('strong')
+            # Jesli tekst elementu jest taki jak zadany w valid_country
+            if option.get_attribute("innerText") == narod:
+                # Przewin do tego elementu
+                option.location_once_scrolled_into_view
+                # Kliknij
+                option.click()
+                # Wyjdz z petli - juz znalazlem i kliknalem
+                break
+
+        dr.find_element_by_xpath(check1).click()
+        #dr.find_element_by_xpath(pole7).send_keys(nar)
+
+        #dr.find_element_by_name(pole3).send_keys(kod_kraju)
+
+
+        #<input type="text" name="phone-number-country-code" maxlength="256" autocomplete="off" placeholder="Wyszukaj…">
+        #driver.find_element_by_id('SubmitCreate').click()
+
+        #Select(driver.find_element_by_id('days')).select_by_value(birthday)
+
+        #email_text=driver.find_element_by_id('email').get_attribute('value')
+        #assert email_text == email
+
+
+        #driver.find_element_by_id('alias').clear()
+        #driver.find_element_by_id('alias').send_keys(aliasss)
+
+        sleep(10)
+        #self.assertIn('Bankowe',driver.title)
+        #print(driver.title)
+
+
+    # sprztanie
+    def tearDown(self):
+        self.driver.quit()
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
+
+
+
+    """"
+
+    """
