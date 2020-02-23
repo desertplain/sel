ead6390173a7f833aee97784cb20916da3fb5e8b
student <tester@tester-m>
2020-02-23 01:34:01 +0100

    dopisanie sel4 w domu

--- sel4.py
+++ sel4.py
@@ -6,9 +6,20 @@ from time import sleep
 import unittest
 
 email='llolla@op.pl'
+fname='misiek'
+lname='misiowy'
+haslo='haslo'
 birthday='3'
 birthmonth='12'
 birthyear='2000'
+adres='radosna 1'
+miasto='wroclaw'
+stan='Alabama'
+kod='55-555'
+kraj='United States'
+fon='121212121'
+aliasss='buda'
+
 print 'hellou mai frend'
 
 # tworzymy klase wspplcheck dziedziczaca po z modulu unittest
@@ -31,16 +42,13 @@ class APRegistration(unittest.TestCase):
 
         driver.find_element_by_id('SubmitCreate').click()
 
-        driver.find_element_by_id('customer_firstname').send_keys('namecc')
+        driver.find_element_by_id('customer_firstname').send_keys(fname)
 
         driver.find_element_by_id('id_gender1').click()
 
-        driver.find_element_by_id('customer_lastname').send_keys('lasname')
+        driver.find_element_by_id('customer_lastname').send_keys(lname)
 
-        email_text=driver.find_element_by_id('email').get_attribute('value')
-        assert email_text == email
-
-        driver.find_element_by_id('passwd').send_keys('haslo')
+        driver.find_element_by_id('passwd').send_keys(haslo)
 
         Select(driver.find_element_by_id('days')).select_by_value(birthday)
 
@@ -48,7 +56,34 @@ class APRegistration(unittest.TestCase):
 
         Select(driver.find_element_by_id('years')).select_by_value(birthyear)
 
+        email_text=driver.find_element_by_id('email').get_attribute('value')
+        assert email_text == email
+        print email_text
 
+        fname_text=driver.find_element_by_id('firstname').get_attribute('value')
+        assert fname_text == fname
+        print fname_text
+
+        lname_text=driver.find_element_by_id('lastname').get_attribute('value')
+        assert lname_text == lname
+        print lname_text
+
+        driver.find_element_by_id('address1').send_keys(adres)
+
+        driver.find_element_by_id('city').send_keys(miasto)
+
+        driver.find_element_by_id('id_state').send_keys(stan)
+
+        driver.find_element_by_id('postcode').send_keys(kod)
+
+        driver.find_element_by_id('id_country').send_keys(kraj)
+
+        driver.find_element_by_id('phone_mobile').send_keys(fon)
+
+        driver.find_element_by_id('alias').clear()
+        driver.find_element_by_id('alias').send_keys(aliasss)
+    
+        driver.find_element_by_id('submitAccount').click()
 
         sleep(10)
         #self.assertIn('Bankowe',driver.title)
